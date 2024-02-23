be9b6e7d4833ce6d8de675bc4193d95958780851
junior <lewisomogi01@gmail.com>
2024-02-23 01:23:37 -0800

    rest commit

--- book_api/views.py
+++ book_api/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 from book_api.models import Book
+from book_api.serializer import BookSerializer
 
 # Create your views here.
+@api_view(['GET'])
 def book_list(request):
-    books = Book.object.all() #complex data
-    books_Python = list (books.values()) #python DS
-    return JsonResponse({
-        'books': books_Python
-    })
-    
+    books = Book.objects.all() #complex data
+    serializer= BookSerializer(books, many=True)
+    return Response(serializer.data)
 
+
+@api_view(['POST'])
+def book_create(request):
+    serializer = BookSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    
+    else:
+        return Response(serializer.errors)
+   
