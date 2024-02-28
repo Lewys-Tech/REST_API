f71251a3b14cf53f521e0035b6387168571f1a31
junior <lewisomogi01@gmail.com>
2024-02-27 19:34:29 -0800

    first commit

--- book_api/views.py
+++ book_api/views.py
@@ -1,43 +1,49 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 from book_api.models import Book
 from book_api.serializer import BookSerializer
 
 # Create your views here.
+
+
 @api_view(['GET'])
 def book_list(request):
-    books = Book.objects.all() #complex data
-    serializer= BookSerializer(books, many=True)
-    return Response(serializer.data)
-
+    books = Book.objects.all()
+    serializer = BookSerializer(books, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def book_create(request):
     serializer = BookSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
-        return Response(serializer.data)
-    
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
-        return Response(serializer.errors)
-    
-@api_view(['GET', 'PUT', 'DELETE'])   
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def book(request, pk):
     try:
         book = Book.objects.get(pk=pk)
     except Book.DoesNotExist:
-        return Response(status=404)
-    
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
     if request.method == 'GET':
         serializer = BookSerializer(book)
-        return Response(serializer.data)
-    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     if request.method == "PUT":
         serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    if request.method == "DELETE":
+        book.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+            
         
    
